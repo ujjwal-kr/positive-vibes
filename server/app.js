95153bb1d3205471e61079e4b70d443154bd55f5
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-07-09 22:11:00 +0530

    setting up basic connection to server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json())
+app.use(cors);
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/positive-vibes', {
     useNewUrlParser: true,
