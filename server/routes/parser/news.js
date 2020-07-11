f4c838dbdeb626af4b6c7c485fad0cae3d5c0960
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-07-11 22:26:36 +0530

    added new news topic routes

--- server/routes/parser/news.js
+++ server/routes/parser/news.js
@@ -50,4 +50,69 @@ router.get('/technology', async (req, res, next) => {
         })
 });
 
+router.get('/health', async (req, res, next) => {
+    await Axios.get('https://news.google.com/rss/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNR3QwTlRFU0FtVnVLQUFQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen', {
+            headers: {"User-agent": UA}
+        }).then(async data => {
+            const result = await convert.xml2json(data.data, {
+                compact: true
+            });
+            const parsed = JSON.parse(result);
+            parsed.rss.channel.item.map(it => {
+                it.description = null;
+                it.guid = null;
+                it.source._attributes = null;
+            })
+            const final = parsed.rss.channel.item;
+            const resl = await Analyzer(final, req.body.score);
+            res.json({resl})
+        }).catch(e => {
+            return e
+        })
+});
+
+router.get('/science', async (req, res, next) => {
+    await Axios.get('https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFp0Y1RjU0FtVnVHZ0pKVGlnQVAB?hl=en-IN&gl=IN&ceid=IN%3Aen', {
+            headers: {"User-agent": UA}
+        }).then(async data => {
+            const result = await convert.xml2json(data.data, {
+                compact: true
+            });
+            const parsed = JSON.parse(result);
+            parsed.rss.channel.item.map(it => {
+                it.description = null;
+                it.guid = null;
+                it.source._attributes = null;
+            })
+            const final = parsed.rss.channel.item;
+            const resl = await Analyzer(final, req.body.score);
+            res.json({resl})
+        }).catch(e => {
+            return e
+        })
+});
+
+router.get('/science', async (req, res, next) => {
+    await Axios.get('https://news.google.com/rss/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRE55YXpBU0FtVnVLQUFQAQ?hl=en-IN&gl=IN&ceid=IN%3Aen', {
+            headers: {"User-agent": UA}
+        }).then(async data => {
+            const result = await convert.xml2json(data.data, {
+                compact: true
+            });
+            const parsed = JSON.parse(result);
+            parsed.rss.channel.item.map(it => {
+                it.description = null;
+                it.guid = null;
+                it.source._attributes = null;
+            })
+            const final = parsed.rss.channel.item;
+            const resl = await Analyzer(final, req.body.score);
+            res.json({resl})
+        }).catch(e => {
+            return e
+        })
+});
+
+
+
 module.exports = router;
