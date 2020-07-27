39a0d711b9e7676899271903d67ae287a9e64710
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-07-27 18:37:09 +0530

    added setting feature in UI

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../../models/userModel');
-const KEY = require('../../settings/key');
+const express = require("express")
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../../models/userModel')
+const KEY = require('../../settings/key')
 
 const UserMiddleware = require('../../middlewares/userMiddleware')
 const { ValidateLogin, ValidateRegister } = require('../../middlewares/auth-validator')
@@ -20,7 +20,7 @@ router.post('/register', ValidateRegister, async(req, res, next) => {
     await bcrypt.genSalt(10, async (err, salt) => {
         await bcrypt.hash(data.password, salt, async (err, hash) => {
             if (err) return next(err)
-            data.password = hash;
+            data.password = hash
             const user = await User.create(data).catch(e => {
                 res.json({message: e})
             })
@@ -33,32 +33,30 @@ router.post('/register', ValidateRegister, async(req, res, next) => {
 router.post('/login', ValidateLogin, async(req, res, next) => {
     const { email, password } = req.body;
     const user = await User.findOne({email})
-    if(!user) return res.status(404).json({message: "Didnt find any user"});
+    if(!user) return res.status(404).json({message: "Didnt find any user"})
     await bcrypt.compare(password, user.password, async (err, correct) => {
         if (err) return console.log(err);
-        if (!correct) return res.status(404).json({message: "Wrong password"});
-        const payload = {
-            id: user.id
-        }   
+        if (!correct) return res.status(404).json({message: "Wrong password"})
+        const payload = { id: user.id }   
         await jwt.sign(payload, KEY, {expiresIn: '7d'}, (err, token) => {
-             if(err) return next(err);
+             if(err) return next(err)
              return res.json({ user, token })
         })
     })
 })
 
 router.patch('/user/:id', UserMiddleware, async (req, res) => {
-    let user = req.body.user;
-    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"});
-    user.setting = req.body.setting;
-    user.save();
+    let user = req.body.user
+    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"})
+    user.setting = req.body.setting
+    user.save()
     res.json({message: "Canged settings to" + req.body.setting})
 })
 
 router.get('/setting/:id', UserMiddleware, async (req, res) => {
-    let user = req.body.user;
-    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"});
+    let user = req.body.user
+    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"})
     return res.json({ setting: user.setting })
 })
 
-module.exports = router;
+module.exports = router
--- src/Settings/settings.jsx
+++ src/Settings/settings.jsx
@@ -2,49 +2,75 @@ import React from 'react';
 import axios from 'axios';
 import URL from '../url';
 
+import { Button } from '@material-ui/core';
+
 class SettingsComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            setting: ''
+            setting: '',
+            basic: false,
+            strict: false,
+            moderate: false
         }
         this.changeSetting = this.changeSetting.bind(this);
     }
 
     async componentDidMount() {
         const user = JSON.parse(localStorage.getItem('user'))
-        console.log(user)
         const token = localStorage.getItem('token');
-        console.log(user._id)
-        await axios.get(URL + 'auth/setting/' +user._id, {
-            headers: { 'authorization': token}
+        await axios.get(URL + 'auth/setting/' + user._id, {
+            headers: { 'authorization': token }
         }).then(res => {
             this.setState({
                 setting: res.data.setting
             })
+            this.changeSettingVal(res.data.setting)
         }).catch(e => {
             console.log(e)
         })
     }
 
-     async changeSetting(str) {
+    async changeSetting(str) {
         const user = JSON.parse(localStorage.getItem('user'))
         const token = localStorage.getItem('token');
-        await axios.patch(URL+'auth/user/'+user._id, {setting: str}, {
-            headers: {'authorization': token}
+        await axios.patch(URL + 'auth/user/' + user._id, { setting: str }, {
+            headers: { 'authorization': token }
         }).then(res => {
             this.setState({
                 setting: str
             })
-            console.log(res)
+            this.changeSettingVal(str)
         }).catch(e => console.log(e))
     }
-    
+
+    changeSettingVal(str) {
+        if (str === 'basic') return this.setState({ basic: true, moderate: false, strict: false })
+        if (str === 'moderate') return this.setState({ moderate: true, basic: false, strict: false })
+        if (str === 'strict') return this.setState({ strict: true, basic: false, moderate: false })
+        return this.setState({ basic: true, moderate: false, strict: false })
+    }
+
 
     render() {
         return (
             <div>
-                <button onClick={() => this.changeSetting('moderate')}>CHANGE</button>
+                <Button 
+                color="primary" 
+                variant={this.state.basic ? 'contained' : ''} 
+                onClick={() => this.changeSetting('basic')}>BASIC</Button> <br />
+                <br/>
+                
+                <Button 
+                color="primary" 
+                variant={this.state.moderate ? 'contained' : ''} 
+                onClick={() => this.changeSetting('moderate')}>MODERATE</Button> <br />
+                <br/>
+
+                <Button 
+                color="primary" 
+                variant={this.state.strict ? 'contained' : ''} 
+                onClick={() => this.changeSetting('strict')}>Strict</Button>
             </div>
         )
     }
