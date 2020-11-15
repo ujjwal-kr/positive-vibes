72ca7f52b6c7f18260d82b8d187362251b30030d
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-11-15 17:35:01 +0530

    fixed search issue

---
+++ src/Components/footer.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Footer = styled.p`
+    padding: 1.3em
+`
--- src/News/search.jsx
+++ src/News/search.jsx
@@ -23,15 +23,16 @@ class SearchComponent extends React.Component {
         const token = localStorage.getItem("token")
         const user = JSON.parse(localStorage.getItem("user"));
 
-        await CheckAuth.check(token).then(res => {
+        const { match: { params } } = this.props;
+        this.setState({ searchTerm: params.query })
+
+        CheckAuth.check(token).then(res => {
             this.setState({ user: user })
         }).catch(e => {
             this.setState({ login: true })
         })
-        const { match: { params } } = this.props;
-        this.setState({ searchTerm: params.query })
 
-        await FetchNews.search(params.query, token).then(data => {
+        FetchNews.search(params.query, token).then(data => {
             if(Object.keys(data.data.resl).length < 1) {
                 return
             } else {
