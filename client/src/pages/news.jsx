0f0f1d4409a441a33af0cb831eb6e0e34697b2eb
Ujjwal <epicujjwal@protonmail.com>
2022-09-12 00:13:00 +0530

    client: load id topics

--- client/src/pages/news.jsx
+++ client/src/pages/news.jsx
@@ -1,10 +1,34 @@
 import { useParams } from "react-router-dom"
 
+import { useEffect, useState } from "react"
+import FetchNews from "../services/news.service"
 
 export default function News() {
     let params = useParams();
 
+    let [news, setNews] = useState([])
+
+    useEffect(() => {
+        fetchNews()
+    }, [])
+
+    async function fetchNews() {
+        let newsRes = await FetchNews.fetchWithId(params.id)
+        setNews(newsRes.data)
+        console.log(newsRes.data)
+    }
+
     return (
-        <div>News: {params.id}</div>
+        <div>
+            <h1>{params.id}</h1>
+            <br />
+
+            {news ?
+
+                news.map(item => <p>{item.title._text}</p>)
+
+                : null}
+
+        </div>
     )
 }
