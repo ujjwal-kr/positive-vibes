d462b14af8cda69b253df61a69dbbede0e9844fa
Ujjwal <epicujjwal@protonmail.com>
2022-09-11 20:09:06 +0530

    client: authservice

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,12 +1,19 @@
 import axios from "axios"
+import URL from "../url"
 
 const AuthService = {
-    login: () => {
-        console.log("henlo")
+    login: async (email, password) => {
+        return await axios.post(`${URL}auth/login`, {email, password})
     }, 
 
-    register: () => {
+    register: async (name, email, password) => {
+        return await axios.post(URL+'auth/register', {name, email, password})
+    },
 
+    checkAuth: async (token) => {
+        return await axios.get(URL+'auth/check', {
+            headers: {'authorization': token}
+        });
     }
 }
 
