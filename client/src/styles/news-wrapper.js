1edb9bcf2d46735cda29be9b945ca52537060c78
Ujjwal <epicujjwal@protonmail.com>
2022-09-19 03:53:14 +0530

    client: push news wrapper

---
+++ client/src/styles/news-wrapper.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const NewsWrapper = styled.div`
+    margin: 1rem;
+    padding: 1rem;
+`
+
+export const LoadWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 5rem;
+`
+export const NewsContentWrapper = styled.div`
+    margin: auto;
+    width: 70%;
+`
