c4e68cab571b446c148934e53f9a7c7b5635a8c7
Ujjwal <epicujjwal@protonmail.com>
2022-09-22 02:15:13 +0530

    server: stop weather from crashing if ip is not defined

--- server/routes/weather.js
+++ server/routes/weather.js
@@ -7,9 +7,12 @@ const {
 
 router.get('/', async (req, res) => {
     let ip = req.headers['x-forwarded-for']
-    if (ip.length > 17) {
-        ip = req.headers['x-forwarded-for'].split(',')[0]
+    if (ip != undefined) {
+        if (ip.length > 17) {
+            ip = req.headers['x-forwarded-for'].split(',')[0]
+        }
     }
+
     const agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
     const url = "https://api.ip8.com/ip/lookup/" + ip;
 
@@ -18,6 +21,7 @@ router.get('/', async (req, res) => {
             headers: { 'user-agent': agent }
         })
         const city = response.data.details.geoip[0].city.name
+        console.log(city);
         try {
             let finalRes = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=84f0c05e16abc57b03ca8fa00b59f78e&units=metric`)
             const temperature = finalRes.data.main.temp
