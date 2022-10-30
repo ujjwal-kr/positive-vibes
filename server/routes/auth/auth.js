660dafa7d4995523ba5ed09011f1f112a6423c57
T S Ajeet <codewithstatus200@gmail.com>
2022-10-30 10:59:14 +0530

    fix ujjwal-kr/positive-vibes#34
    1. Migrate from callback to async-await in auth.js
    2. Make a new file .example.env to document and pass envs required for running server
    3. Update README.md with the bulid changes

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,73 +1,76 @@
-const express = require("express")
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const User = require('../../models/userModel')
-const KEY = require('../../settings/key')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/userModel');
+const KEY = require('../../settings/key');
 
-const UserMiddleware = require('../../middlewares/userMiddleware')
-const { ValidateLogin, ValidateRegister } = require('../../middlewares/auth-validator')
+const UserMiddleware = require('../../middlewares/userMiddleware');
+const {
+  ValidateLogin,
+  ValidateRegister,
+} = require('../../middlewares/auth-validator');
 
-router.post('/register', ValidateRegister, async(req, res, next) => {
+router.post('/register', ValidateRegister, async (req, res, next) => {
+  try {
     const data = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    const {email} = req.body
-    const user = await User.findOne({email})
-    if(user) return res.status(400).json({message: "User Exists"})
-    bcrypt.genSalt(10, async (err, salt) => {
-        bcrypt.hash(data.password, salt, async (err, hash) => {
-            if (err) return next(err)
-            data.password = hash
-            const user = await User.create(data).catch(e => {
-                res.json({message: e})
-            })
-            return res.status(201).json({user})
-        })
-    })
-})
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    if (await User.findOne({ email: data.email }))
+      return res.status(400).json({ message: 'User Exists' });
+    const SALT = bcrypt.genSaltSync(10);
+    data.password = bcrypt.hashSync(data.password, SALT);
+    const user = await User.create(data);
+    return res.status(201).json({ user });
+  } catch (error) {
+    next(error);
+  }
+});
 
+router.post('/login', ValidateLogin, async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-router.post('/login', ValidateLogin, async(req, res, next) => {
-    const { email, password } = req.body
-    const user = await User.findOne({email})
-
-    if(!user) return res.status(404).json({message: "Didnt find any user"})
-    bcrypt.compare(password, user.password, async (err, correct) => {
-        if (err) return console.log(err)
-        if (!correct) return res.status(404).json({message: "Wrong password"})
-
-        const payload = { id: user.id }   
-        jwt.sign(payload, KEY, {expiresIn: '30d'}, (err, token) => {
-             if(err) return next(err)
-             return res.json({ user, token })
-        })
-    })
-})
+    if (!user)
+      return res.status(401).json({ message: 'Incorrect credentials' });
+    const validate = bcrypt.compareSync(password, user.password);
+    if (!validate)
+      return res.status(401).json({ message: 'Incorrect credentials' });
+    const payload = { id: user.id };
+    const token = await jwt.sign(payload, KEY, { expiresIn: '30d' });
+    return res.json({ user, token });
+  } catch (error) {
+    return next(error);
+  }
+});
 
 router.patch('/user/:id', UserMiddleware, async (req, res) => {
-    let user = req.body.user
-    if (!user) return res.status(403).json({message: "UNAUTHORIZED"})
-    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"})
+  let user = req.body.user;
+  if (!user) return res.status(403).json({ message: 'UNAUTHORIZED' });
+  if (req.params.id !== user.id)
+    return res.status(403).json({ message: 'UNAUTHORIZED' });
 
-    user.setting = req.body.setting
-    user.save()
-    res.json({message: "Changed settings to " + req.body.setting})
-})
+  user.setting = req.body.setting;
+  user.save();
+  res.json({ message: 'Changed settings to ' + req.body.setting });
+});
 
 router.get('/check', UserMiddleware, async (req, res) => {
-    if (req.body.user == null) return res.status(403).json({message: "UNAUTHORIZED"});
-    return res.status(200).json({message: "Looks Alright"})
-})
+  if (req.body.user == null)
+    return res.status(403).json({ message: 'UNAUTHORIZED' });
+  return res.status(200).json({ message: 'Looks Alright' });
+});
 
 router.get('/setting/:id', UserMiddleware, async (req, res) => {
-    let user = req.body.user
-    if (!user) return res.status(403).json({message: "UNAUTHORIZED"})
-    if (req.params.id !== user.id) return res.status(403).json({message: "UNAUTHORIZED"})
+  let user = req.body.user;
+  if (!user) return res.status(403).json({ message: 'UNAUTHORIZED' });
+  if (req.params.id !== user.id)
+    return res.status(403).json({ message: 'UNAUTHORIZED' });
 
-    return res.json({ setting: user.setting })
-})
+  return res.json({ setting: user.setting });
+});
 
-module.exports = router
+module.exports = router;
